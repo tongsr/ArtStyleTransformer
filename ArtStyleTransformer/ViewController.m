e6eacfa402fbacaa0d97cb9879819e1ba4a20417
franky <493860286@qq.com>
2019-07-02 23:47:26 +0800

    add init

---
+++ ArtStyleTransformer/ViewController.m
@@ -0,0 +1,272 @@
+//
+//  ViewController.m
+//  ArtStyleTransformer
+//
+//  Created by terence on 2019/7/1.
+//  Copyright © 2019年 terence. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "la_muse.h"
+#import "rain_princess.h"
+#import "udnie.h"
+#import "wave.h"
+#import <CoreML/CoreML.h>
+#import <Vision/Vision.h>
+#import <UIKit/UIKit.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <Photos/Photos.h>
+#define WeakSelf __weak typeof(self) weakSelf = self;
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIImage *image=[UIImage imageNamed:@"imgtest.jpeg"];
+    [self changeImage:image toStyle:1];
+    
+}
+
+
+    
+    
+    
+-(void)changeImage:(UIImage *)image toStyle:(int)style{
+    CGSize osize = image.size;
+    CVPixelBufferRef img = [self scaleToSize:image size:CGSizeMake(883, 720)];
+    
+
+    
+    
+    VNCoreMLModel *vnCoreMMModel;
+    NSError *error = nil;
+    
+    
+    if (style==0) {
+        la_muse *model = [[la_muse alloc]init];
+        la_museInput *input = [[la_museInput alloc]initWithImg_placeholder__0:img];
+        
+        la_museOutput *output =[model predictionFromFeatures:input error:nil];
+        
+        UIImage *resultImage = [self imageFromPixelBuffer:[output featureValueForName:@"add_37__0"].imageBufferValue];
+        UIImageView *imgView = [[UIImageView alloc]initWithImage:resultImage];
+        imgView.frame=CGRectMake(10, 10, osize.width, osize.height);
+        [self.view addSubview:imgView];
+        
+    }
+    if (style==1) {
+        rain_princess *model = [[rain_princess alloc]init];
+        rain_princessInput *input = [[rain_princessInput alloc]initWithImg_placeholder__0:img];
+        
+        rain_princessOutput *output =[model predictionFromFeatures:input error:nil];
+        
+        UIImage *resultImage = [self imageFromPixelBuffer:[output featureValueForName:@"add_37__0"].imageBufferValue];
+        UIImageView *imgView = [[UIImageView alloc]initWithImage:resultImage];
+        imgView.frame=CGRectMake(10, 10, osize.width, osize.height);
+        [self.view addSubview:imgView];
+    }
+    if (style==2) {
+        udnie *model = [[udnie alloc]init];
+        vnCoreMMModel = [VNCoreMLModel modelForMLModel:model.model error:&error];
+    }
+    if (style==3) {
+        wave *model = [[wave alloc]init];
+        vnCoreMMModel = [VNCoreMLModel modelForMLModel:model.model error:&error];
+
+    }
+
+    
+    
+    
+    
+    
+    // 创建处理requestHandler
+    VNImageRequestHandler *handler = [[VNImageRequestHandler alloc] initWithCGImage:img options:@{}];
+    
+    WeakSelf
+    // 创建request
+    VNCoreMLRequest *request = [[VNCoreMLRequest alloc] initWithModel:vnCoreMMModel completionHandler:^(VNRequest * _Nonnull request, NSError * _Nullable error) {
+        
+        //CGImageRef image=[weakSelf imageFromCVPixelBufferRef0:request.results[0]];
+        NSArray *array = request.results;
+        NSLog([array objectAtIndex:0]);
+
+        //CFBridgingRetain([request.results objectAtIndex:0]);
+
+    }];
+    
+    
+    
+    // 发送识别请求
+    [handler performRequests:@[request] error:&error];
+    if (error) {
+        NSLog(@"%@",error.localizedDescription);
+    }
+}
+
+    
+    
+    
+
+    
+
+    
+    //改变图片的size
+- (CVPixelBufferRef)scaleToSize:(UIImage *)img size:(CGSize)size{
+    // 创建一个bitmap的context
+    // 并把它设置成为当前正在使用的context
+    UIGraphicsBeginImageContext(size);
+    // 绘制改变大小的图片
+    [img drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    // 从当前context中创建一个改变大小后的图片
+    UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    // 使当前的context出堆栈
+    UIGraphicsEndImageContext();
+    // 返回新的改变大小后的图片
+    
+    return [self makePixelBufferFromCGImage:scaledImage.CGImage];
+    //return scaledImage;
+    
+    
+}
+    
+    
+    
+    
+    
+    
+    
+- (UIImage *)imageFromPixelBuffer:(CVPixelBufferRef)pixelBufferRef {
+    CVImageBufferRef imageBuffer =  pixelBufferRef;
+    size_t width = CVPixelBufferGetWidth(imageBuffer);
+    size_t height = CVPixelBufferGetHeight(imageBuffer);
+    
+    
+    CVPixelBufferLockBaseAddress(imageBuffer, 0);
+    void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
+    
+    size_t bufferSize = CVPixelBufferGetDataSize(imageBuffer);
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 0);
+    
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, baseAddress, bufferSize, NULL);
+    
+    CGImageRef cgImage = CGImageCreate(width, height, 8, 32, bytesPerRow, rgbColorSpace, kCGImageAlphaNoneSkipFirst | kCGBitmapByteOrderDefault, provider, NULL, true, kCGRenderingIntentDefault);
+    UIImage *image = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(rgbColorSpace);
+    CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
+    return image;
+}
+    
+    
+    
+    
+    
+    
+/***
+    private func pixelBuffer(cgImage: CGImage, width: Int, height: Int) -> CVPixelBuffer {
+        var pixelBuffer: CVPixelBuffer? = nil
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+        if status != kCVReturnSuccess {
+            fatalError("Cannot create pixel buffer for image")
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
+        let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.noneSkipFirst.rawValue)
+        let context = CGContext(data: data, width: width, height: height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
+        
+        context?.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer!
+    }
+***/
+
+    
+    
+-(CVPixelBufferRef)makePixelBufferFromCGImage:(CGImageRef)cgImage{
+    NSDictionary *options = @{
+                              (NSString*)kCVPixelBufferCGImageCompatibilityKey : @YES,
+                              (NSString*)kCVPixelBufferCGBitmapContextCompatibilityKey : @YES,
+                              (NSString*)kCVPixelBufferIOSurfacePropertiesKey: [NSDictionary dictionary]
+                              };
+    CVPixelBufferRef pxbuffer = NULL;
+    CGFloat frameWidth = CGImageGetWidth(cgImage);
+    CGFloat frameHeight = CGImageGetHeight(cgImage);
+
+    //let status = CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+    CVReturn status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                          frameWidth,
+                                          frameHeight,
+                                          kCVPixelFormatType_32BGRA,
+                                          nil,
+                                          &pxbuffer);
+    if (status != kCVReturnSuccess) {
+        NSLog(@"Cannot create pixel buffer for image");
+    }
+
+    CVPixelBufferLockBaseAddress(pxbuffer, 0);
+    void *data = CVPixelBufferGetBaseAddress(pxbuffer);
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+
+    
+    CGBitmapInfo bitmapInfo = kCGBitmapByteOrder32Little|kCGImageAlphaFirst;
+
+    
+    CGContextRef context = CGBitmapContextCreate(data,
+                                                 frameWidth,
+                                                 frameHeight,
+                                                 8,
+                                                 CVPixelBufferGetBytesPerRow(pxbuffer),
+                                                 rgbColorSpace,
+                                                 bitmapInfo);
+
+    //NSParameterAssert(context);
+    CGContextDrawImage(context, CGRectMake(0, 0, frameWidth, frameHeight), cgImage);
+    CVPixelBufferUnlockBaseAddress(pxbuffer, 0);
+    CGColorSpaceRelease(rgbColorSpace);
+    CGContextRelease(context);
+    return pxbuffer;
+
+}
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //other
+    
+- (UIImage *)convert:(CVPixelBufferRef)pixelBuffer {
+    CIImage *ciImage = [CIImage imageWithCVPixelBuffer:pixelBuffer];
+    
+    CIContext *temporaryContext = [CIContext contextWithOptions:nil];
+    CGImageRef videoImage = [temporaryContext
+                             createCGImage:ciImage
+                             fromRect:CGRectMake(0, 0, CVPixelBufferGetWidth(pixelBuffer), CVPixelBufferGetHeight(pixelBuffer))];
+    
+    UIImage *uiImage = [UIImage imageWithCGImage:videoImage];
+    CGImageRelease(videoImage);
+    
+    return uiImage;
+}
+
+@end
---
+++ ArtStyleTransformer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ArtStyleTransformer
+//
+//  Created by terence on 2019/7/1.
+//  Copyright © 2019年 terence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
